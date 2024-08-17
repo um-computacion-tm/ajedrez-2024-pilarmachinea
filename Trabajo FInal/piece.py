aa2e5d0686e06c70cecccb2c3a8444cf2c513e8e
pilar machinea <m.machinea@alumno.um.edu.ar>
2024-08-17 17:53:47 -0300

    tercer entrega

--- Trabajo FInal/piece.py
+++ Trabajo FInal/piece.py
@@ -3,19 +3,25 @@ class Piece:
         self.__color__ = color
 
 class Rook(Piece):  
-    ...
+    def __init__(self, color):
+        super().__init__(color)
 
 class Pawn(Piece):
-    ...
+    def __init__(self, color):
+        super().__init__(color)
 
 class Knight(Piece):
-    ...
+    def __init__(self, color):
+        super().__init__(color)
 
 class Bishop(Piece):
-    ...
+    def __init__(self, color):
+        super().__init__(color)
 
 class Queen(Piece):
-    ...
+    def __init__(self, color):
+        super().__init__(color)
     
 class King(Piece):
-    ...
+    def __init__(self, color):
+        super().__init__(color)
