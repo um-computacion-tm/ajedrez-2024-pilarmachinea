55f5021edbedb17f4820630a10665bf7bdac681f
pilar machinea <m.machinea@alumno.um.edu.ar>
2024-08-19 23:04:53 -0300

    agrego bloqueo movimiento pieza

--- Trabajo FInal/piece.py
+++ Trabajo FInal/piece.py
@@ -2,6 +2,10 @@ class Piece:
     def __init__(self, color):
         self.__color__ = color
 
+    # verifica un movimiento especifico es valido
+    def is_valid_move(self, board, start, end):
+        raise NotImplementedError
+
 class Rook(Piece):  
     def __init__(self, color):
         super().__init__(color)
