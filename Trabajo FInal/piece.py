298cc008e966204b4a158830c0b0ac56372f3243
pilar machinea <m.machinea@alumno.um.edu.ar>
2024-09-17 23:27:51 -0300

    piece

--- Trabajo FInal/piece.py
+++ Trabajo FInal/piece.py
@@ -1,28 +1,16 @@
 class Piece:
-    def __init__(self, color):
+    def __init__(self, color, name):
         self.__color__ = color
+        self.__name__ = name
 
     def __str__(self):
-        ...
+        return self.white_str_ if self.__color__ == 'white' else self.black_str
+    
+    def color(self):
+        return self.__color__
+    
+    def name(self):
+        return self.__name__
 
-    # verifica un movimiento especifico es valido
-    def is_valid_move(self, board, start, end):
-        raise NotImplementedError
 
-class Rook(Piece):  
-    ...
 
-class Knight(Piece):
-    ...
-
-class Bishop(Piece):
-    ...
-
-class Queen(Piece):
-     ...
-
-class King(Piece):
-      ...
-
-class Pawn(Piece):
-    ...
