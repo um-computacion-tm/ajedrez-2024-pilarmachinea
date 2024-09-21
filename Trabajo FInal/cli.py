b39cfd5bed1be99277e189a805a0de58e1779807
pilar machinea <m.machinea@alumno.um.edu.ar>
2024-09-20 22:17:42 -0300

    cli

--- Trabajo FInal/cli.py
+++ Trabajo FInal/cli.py
@@ -9,3 +9,35 @@ class Cli:
         board = chess.get_board()
         position = board.get_position()
         return [chess, position]
+    
+    def comenzar(self,chess, positions):
+        print("Para salir escriba QUIT.")
+        print("Turno: " + chess.get_turn())
+        print("   " + "   ".join([str(i) for i in range(8)]))
+        print("  " + "----" * 8) 
+
+        for i, fila in enumerate(positions):
+            fila_str = ' | '.join([str(piece) if piece else ' ' for piece in fila])
+            print(f'{i} | {fila_str} |') 
+            print("  " + "----" * 8)  
+
+
+    def input_function(self):
+        from_row = int(input('From row: '))
+
+            # Si el usuario introduce 999, significa que se quiere salir del juego
+        if from_row == 999:
+                return False
+
+        Cli.buscar_index_error(self,from_row)
+
+        from_col = int(input('From col: '))
+        Cli.buscar_index_error(self,from_col)
+
+        to_row = int(input('To row: '))
+        Cli.buscar_index_error(self,to_row)
+
+        to_col = int(input('To col: '))
+        Cli.buscar_index_error(self,to_col)   
+
+        return [from_row, from_col, to_row, to_col]
